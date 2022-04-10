7872b45bf3cc3000de08c20907ab92bec8e83030
ASUS <ASUS@192.168.0.3>
2022-04-10 16:00:32 +0900

    2022-04-10 C1

---
+++ src/Practice/test.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class test {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		double result = (double) (x / y);
+		System.out.println(result);
+	}
+
+}
